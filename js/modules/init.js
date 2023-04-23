1896bb385c9d6924147b5af909a7846f0dcaa685
RalphsCode <124472641+RalphsCode@users.noreply.github.com>
2023-04-22 23:41:23 -0700

    Add files via upload

--- js/modules/init.js
+++ js/modules/init.js
@@ -1,1 +1,10 @@
-test
+export async function employees() {                                         // CREATE AND EXPORT AN ASYNC FUNCTION
+    try {
+        const response = await fetch('/js/data/employees.json');            // SITE ROOT RELATIVE PATH
+        const employeeArray = await response.json();
+        return employeeArray;
+                       
+    } catch (error) {
+        console.error(error);                                               // FUNCTION IS IN MODULE FOLDER
+                    }
+                                };
